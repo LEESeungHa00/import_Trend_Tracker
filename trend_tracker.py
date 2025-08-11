6c612bf1c0b76863a2412a64888fea6ac410d6e9
SeungHa <devseungha000@gmail.com>
2025-08-11 16:18:46 +0900

    Update trend_tracker.py

--- trend_tracker.py
+++ trend_tracker.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib
 from datetime import datetime
 import gspread
 from google.oauth2.service_account import Credentials
